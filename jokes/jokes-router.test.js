4dd5e525e81a0556c1322348f82126b418fe58df
Nolan Picini <nolanpic@gmail.com>
2020-02-28 10:57:45 -0800

    Add more tests for jokes router

--- jokes/jokes-router.test.js
+++ jokes/jokes-router.test.js
@@ -1,7 +1,12 @@
 const request = require('supertest');
 const server = require('../api/server');
+const db = require('../database/dbConfig');
 
 describe('Jokes router', () => {
+    beforeEach(async () => {
+        await db('users').truncate();
+    });
+
     describe('GET /', () => {
         test('responds with 401 without token', async () => {
             const res = await request(server).get('/api/jokes');
@@ -14,6 +19,32 @@ describe('Jokes router', () => {
 
             expect(res.body.you).toBeDefined();
             expect(res.body.you).toBe('shall not pass!');
+        });
+
+        test('responds with jokes when a valid token is given', async () => {
+            const user = {
+                username: 'Bill',
+                password: 'thisisaREALLYGoodPwD'
+            };
+
+            // create user
+            await request(server)
+                .post('/api/auth/register')
+                .send(user);
+            
+            // get token
+            const res = await request(server)
+                .post('/api/auth/login')
+                .send(user);
+
+            const { token } = res.body;
+
+            const jokesRes = await request(server)
+                .get('/api/jokes')
+                .set('Authorization', token);
+
+            expect(jokesRes.status).toBe(200);
+            expect(Array.isArray(jokesRes.body)).toBe(true);
         });
     });
 });
