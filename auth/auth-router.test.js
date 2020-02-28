248719b9485f7abbd838303cb2e1589e05f3d918
Nolan Picini <nolanpic@gmail.com>
2020-02-28 10:44:38 -0800

    Add more auth tests

--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -31,6 +31,48 @@ describe('Users router', () => {
             expect(res.type).toMatch(/json/i);
             expect(res.body.token).toBeDefined();
         });
+
+        test('creates a user', async () => {
+            const user = {
+                username: 'Jared',
+                password: 'thisisaREALLYGoodPwD'
+            };
+            await request(server)
+                .post('/api/auth/register')
+                .send(user);
+            
+            const userWithThisUsername = await Users.getByUsername(user.username);
+            expect(userWithThisUsername).toBeDefined();
+            expect(userWithThisUsername.id).toBeDefined();
+        });
+
+        test('responds with 400 if body is invalid', async () => {
+            const user = {
+                username: '',
+                password: ''
+            };
+            const res = await request(server)
+                .post('/api/auth/register')
+                .send(user);
+            
+            expect(res.status).toBe(400);
+        });
+
+        test('responds with 409 conflict if username is taken', async () => {
+            const user = {
+                username: 'Jared',
+                password: 'thisisaREALLYGoodPwD'
+            };
+
+            await Users.create(user);
+
+            const res = await request(server)
+                .post('/api/auth/register')
+                .send(user);
+
+            expect(res.status).toBe(409);
+            expect(res.body.error).toBeDefined();
+        });
     });
 
     describe('POST /api/auth/login', () => {
@@ -65,6 +107,34 @@ describe('Users router', () => {
             
             expect(res.type).toMatch(/json/i);
             expect(res.body.token).toBeDefined();
+        });
+
+        test('responds with 400 if body is invalid', async () => {
+            const user = {
+                username: '',
+                password: ''
+            };
+
+            const res = await request(server)
+                .post('/api/auth/login')
+                .send(user);
+
+            expect(res.status).toBe(400);
+            expect(res.body.error).toBeDefined();
+        });
+
+        test('responds with 401 if credentials are invalid', async () => {
+            const user = {
+                username: 'Jared',
+                password: 'thisisaREALLYGoodPwD'
+            };
+
+            const res = await request(server)
+                .post('/api/auth/login')
+                .send(user);
+            
+            expect(res.status).toBe(401);
+            expect(res.body.error).toBeDefined();
         });
     })
 });
